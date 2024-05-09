2bbe386b21b730d5e6a45c9f547b39597d2d5aa4
aliflazuardi <aliflazuardi250999@gmail.com>
2024-05-09 21:55:16 +0700

    add http listen server 8080:

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to EniQilo Store")
+
+
+  // http server on port 8080
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Welcome to EniQilo Store")
+  })
+  http.ListenAndServe(":8080", nil)
 }
